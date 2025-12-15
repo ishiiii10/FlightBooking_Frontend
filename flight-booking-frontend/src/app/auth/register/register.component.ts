58ccdfcfc8f8a39c280be9b7dd0bc3e616eef2e3
Ishika <22052461@kiit.ac.in>
2025-12-15 17:44:55 +0530

    register component web page overview

--- flight-booking-frontend/src/app/auth/register/register.component.ts
+++ flight-booking-frontend/src/app/auth/register/register.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './register.component.html'
 })
 export class RegisterComponent {
+  registerForm;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.minLength(6)]
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Register Data:', this.registerForm.value);
+    }
+  }
 }
