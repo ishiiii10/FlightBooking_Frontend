d2370b3f2e3d62b5a9618340855279b1f8209c10
Ishika <22052461@kiit.ac.in>
2025-12-15 21:43:48 +0530

    added Style to register

--- flight-booking-frontend/src/app/auth/register/register.component.ts
+++ flight-booking-frontend/src/app/auth/register/register.component.ts
@@ -1,39 +1,38 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
-  templateUrl: './register.component.html'
+  imports: [FormsModule, RouterModule],
+  templateUrl: './register.html',
+  styleUrls: ['./register.css']
 })
-export class RegisterComponent {
+export class Register {
 
-  registerForm;
-  successMessage = '';
+  username = '';
+  password = '';
 
   constructor(
-    private fb: FormBuilder,
+    private authService: AuthService,
     private router: Router
-  ) {
-    this.registerForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.minLength(6)]
+  ) {}
+
+  register() {
+    this.authService.signup({
+      username: this.username,
+      password: this.password
+    }).subscribe({
+      next: () => {
+        alert('Registration successful');
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        console.error(err);
+        alert('Registration failed');
+      }
     });
-  }
-
-  onSubmit() {
-    if (this.registerForm.invalid) return;
-
-    console.log('Mock Register Data:', this.registerForm.value);
-
-    this.successMessage = 'Registration successful (Mock)';
-
-    setTimeout(() => {
-      this.router.navigate(['/login']);
-    }, 1000);
   }
 }
