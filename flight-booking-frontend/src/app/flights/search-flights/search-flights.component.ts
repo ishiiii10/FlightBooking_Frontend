d84d4a4deedd47757a1d5616129c4055ca985a6b
Ishika <22052461@kiit.ac.in>
2025-12-15 17:46:14 +0530

    search flight script added

--- flight-booking-frontend/src/app/flights/search-flights/search-flights.component.ts
+++ flight-booking-frontend/src/app/flights/search-flights/search-flights.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-flights',
-  imports: [],
-  templateUrl: './search-flights.component.html',
-  styleUrl: './search-flights.component.css'
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './search-flights.component.html'
 })
 export class SearchFlightsComponent {
-
+  flights = [
+    { flightNumber: 'AI101', source: 'DEL', destination: 'BOM', price: 5000 },
+    { flightNumber: 'AI202', source: 'BLR', destination: 'DEL', price: 4500 }
+  ];
 }
