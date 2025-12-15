fe9832d8bb0e078f15dd2718a3745cf02acdfd1b
Ishika <22052461@kiit.ac.in>
2025-12-15 17:40:10 +0530

    routes configured

--- flight-booking-frontend/src/app/app.routes.ts
+++ flight-booking-frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { SearchFlightsComponent } from './flights/search-flights/search-flights.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'search-flights', component: SearchFlightsComponent }
+];
