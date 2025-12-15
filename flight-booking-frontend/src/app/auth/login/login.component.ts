5b89904b4daf2c51fa422f9944d132dd79760d71
Ishika <22052461@kiit.ac.in>
2025-12-15 17:43:18 +0530

    login script added

--- flight-booking-frontend/src/app/auth/login/login.component.ts
+++ flight-booking-frontend/src/app/auth/login/login.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html'
 })
 export class LoginComponent {
+  loginForm;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Login Data:', this.loginForm.value);
+    }
+  }
 }
